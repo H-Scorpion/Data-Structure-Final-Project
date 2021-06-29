a78de7fa4b2fe4e793fa12d972df948c02da861a
YellowJason <jason90814@gmail.com>
2021-06-29 16:50:39 +0800

    append self.weighted

--- School.py
+++ School.py
@@ -1,8 +1,9 @@
 from AcceptedStudentHeap import AcceptedStudentHeap
 class School():
-    def __init__(self,schoolName,quota):
+    def __init__(self,schoolName,quota,weighted):
         self.name = schoolName
         self.quota = quota
         self.accept = AcceptedStudentHeap()
+        self.weighted = weighted
     def __str__(self):
         return str(self.name)
