77cc2332e6aad47a4f2926e43208bb6abcf104c8
YellowJason <jason90814@gmail.com>
2021-06-29 17:13:08 +0800

    add schoolweighted

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ with open("input_2.json",'r') as f:
     data=json.load(f)
     schoolList=data['schoolList']
     acceptQuota=data['acceptQuota']
-
+    schoolweighted=data['schoolweighted']
     studentList=data['studentList']
     score=data['score']
     choice=data['choice']
@@ -19,10 +19,11 @@ if __name__=="__main__":
     # Put all the school and student data in to the class Match
     match = Match()
     assert len(schoolList)==len(acceptQuota)
+    assert len(schoolList)==len(schoolweighted)
     assert len(studentList)==len(score)
     assert len(studentList)==len(choice)
     for i in range(len(schoolList)):
-        match.schoolList[schoolList[i]] = School(schoolList[i],acceptQuota[i])
+        match.schoolList[schoolList[i]] = School(schoolList[i],acceptQuota[i],schoolweighted[i])
     for i in range(len(studentList)):
         match.studentQueue.append(Student(studentList[i],score[i],choice[i]))
     match.start_match()
