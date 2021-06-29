9194ab480454cc2f6b036fc786404a5101de1b94
YellowJason <jason90814@gmail.com>
2021-06-29 16:56:30 +0800

    add weighted in generateData

--- generateData.py
+++ generateData.py
@@ -7,16 +7,18 @@ choiceNum=5
 
 schoolList=[str(i) for i in range(1,schoolNum+1)]
 acceptQuota=[random.randint(1,studentNum*.3) for _ in range(schoolNum)]
+schoolweighted = [[random.randint(0,3) for i in range(5)] for _ in range(schoolNum)]
 # acceptQuota=[1 for _ in range(schoolNum)]
 
 studentList=[str(i) for i in range(1,studentNum+1)]
 score=random.sample(range(100), studentNum) #分數不重複
 choice=[random.sample(schoolList,choiceNum) for _ in range(studentNum)]
 
-print('school'+'\t'+'Quota')
+print('school'+'\t'+'Quota'+'\t'+'weighted')
 for i in range(len(schoolList)):
     print(schoolList[i],end = '\t')
-    print(acceptQuota[i])
+    print(acceptQuota[i],end = '\t')
+    print(schoolweighted[i])
 print('student'+'\t'+'score'+'\t'+'choice')
 for i in range(len(studentList)):
     print(studentList[i],end = '\t')
