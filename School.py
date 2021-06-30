94aae4d34de2d345ff6dc1b4b56978d53b4e0e9c
YellowJason <jason90814@gmail.com>
2021-06-30 22:03:58 +0800

    add find_overquotq

--- School.py
+++ School.py
@@ -13,3 +13,11 @@ class School():
         for i in range(len(self.weighted)):
             weighted_score += self.weighted[i]*student.score[i]
         return weighted_score
+    #回傳 1 代表 student_1 超額比序輸了
+    def find_overquota(self,student_1,student_2):
+        i = 0
+        while i <= 4:
+            subject = self.overquota[i]
+            if student_1.score[subject] < student_1.score[subject]:
+                return 1
+        return 0
