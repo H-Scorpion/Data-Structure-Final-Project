b30e8c8bcb52eb0033973573f3c2e3cf23bb4137
H-Scorpion <57944687+H-Scorpion@users.noreply.github.com>
2021-07-01 00:21:26 +0800

    Fix:School find_overquota student compare

--- School.py
+++ School.py
@@ -17,10 +17,15 @@ class School():
     def find_overquota(self,student_1,student_2):
         i = 0
         #完全同分則增額錄取
-        if student_1.score == student_1.score:
-            return 2
+        # I don't quite understand what this means
+        if student_1.score == student_2.score:
+            # return 2
+            return 0
         while i <= 4:
             subject = self.overquota[i]
-            if student_1.score[subject] < student_1.score[subject]:
+            # run for each subject
+            # if studentLow < studentNow then kick studentLow out
+            if student_1.score[subject] < student_2.score[subject]:
                 return 1
+                # return 1 means studentNow wins
         return 0
