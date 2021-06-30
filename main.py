1f62604e085984f03690ec1e9654e0a85e139cc0
H-Scorpion <57944687+H-Scorpion@users.noreply.github.com>
2021-06-30 15:18:57 +0800

    Restrucutre:main ,for better calculatioin of runtime

--- main.py
+++ main.py
@@ -1,41 +1,45 @@
 import json
 import argparse
+import time
 from Match import Match
 from School import School   
 from Student import Student
 
-
-
-
-
-if __name__=="__main__":
-    # Initialize
-    # Put all the school and student data in to the class Match
+def parseArg():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', default='input_2.json')
-
     args = parser.parse_args()
+    return args
 
+def readData():
     with open(args.input,'r') as f:
         data=json.load(f)
-        schoolList=data['schoolList']
-        acceptQuota=data['acceptQuota']
-        schoolweighted=data['schoolweighted']
-        studentList=data['studentList']
-        score=data['score']
-        choice=data['choice']
 
+    assert len(data['schoolList'])==len(data['acceptQuota'])        
+    assert len(data['schoolList'])==len(data['schoolweighted'])
+    assert len(data['studentList'])==len(data['score'])
+    assert len(data['studentList'])==len(data['choice'])
+    return data
+
+def initialize(data):
+    # Initialize
+    # Put all the school and student data into the class Match
+    schoolList=data['schoolList']
+    acceptQuota=data['acceptQuota']
+    schoolweighted=data['schoolweighted']
+    studentList=data['studentList']
+    score=data['score']
+    choice=data['choice']
 
     match = Match()
-    assert len(schoolList)==len(acceptQuota)
-    assert len(schoolList)==len(schoolweighted)
-    assert len(studentList)==len(score)
-    assert len(studentList)==len(choice)
+
     for i in range(len(schoolList)):
         match.schoolList[schoolList[i]] = School(schoolList[i],acceptQuota[i],schoolweighted[i])
     for i in range(len(studentList)):
         match.studentQueue.append(Student(studentList[i],score[i],choice[i]))
-    match.start_match()
+    return match
+
+def printResult(match):
     #列印榜單
     for i in match.schoolList.keys():
         print('college',i,end = '\t')
@@ -43,6 +47,21 @@ if __name__=="__main__":
         for j in match.schoolList[i].accept.array:
             print(j,end = ' ')
         print('')
+
+if __name__=="__main__":
+
+    args = parseArg()
+    schoolList,acceptQuota,schoolweighted,studentList,score,choice=readData()
+    match=initialize(readData())
+
+    t1=time.time()
+    match.start_match()
+    t2=time.time()
+
+    printResult(match)    
+
+    print("runtime:",t2-t1)
+
 
 
     
