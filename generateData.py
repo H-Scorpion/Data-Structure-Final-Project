bff26722daf1e2709ecab46481b03b0da36c788f
YellowJason <jason90814@gmail.com>
2021-06-30 21:49:27 +0800

    add overquota

--- generateData.py
+++ generateData.py
@@ -8,6 +8,8 @@ def genData(schoolNum,studentNum,choiceNum):
     acceptQuota = [random.randint(1,studentNum*.3) for _ in range(schoolNum)]
     #5科加權比重
     schoolweighted = [[random.randint(0,3) for i in range(5)] for _ in range(schoolNum)]
+    #超額比序項目('01234'代表'國英數自社')
+    overquota = [random.sample([0,1,2,3,4],5) for _ in range(schoolNum)]
     # acceptQuota=[1 for _ in range(schoolNum)]
 
     studentList = [str(i) for i in range(1,studentNum+1)]
@@ -18,6 +20,7 @@ def genData(schoolNum,studentNum,choiceNum):
         'schoolList':schoolList,
         'acceptQuota':acceptQuota,
         'schoolweighted':schoolweighted,
+        'overquota':overquota,
         'studentList':studentList,
         'score':score,
         'choice':choice
@@ -28,12 +31,14 @@ def genData(schoolNum,studentNum,choiceNum):
 
 def printData(data):
     schoolList,acceptQuota,schoolweighted=data['schoolList'],data['acceptQuota'],data['schoolweighted']
+    overquota = data['overquota']
     studentList,score,choice=data['studentList'],data['score'],data['choice']
-    print('school'+'\t'+'Quota'+'\t'+'weighted')
+    print('school'+'\t'+'Quota'+'\t'+'weighted'+'\t'+'overquota')
     for i in range(len(schoolList)):
         print(schoolList[i],end = '\t')
         print(acceptQuota[i],end = '\t')
-        print(schoolweighted[i])
+        print(schoolweighted[i],end = '\t')
+        print(overquota[i])
     print('student'+'\t'+'score'+'\t'+'choice')
     for i in range(len(studentList)):
         print(studentList[i],end = '\t')
@@ -52,7 +57,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     data = genData(int(args.schoolNum),int(args.studentNum),int(args.choiceNum))
-
+    printData(data)
+    
     ret=json.dumps(data)
     with open(args.output,'w') as f:
         f.write(ret)
