1cee8b33a7494c8ae398700a62fbbaf5eed97ec0
YellowJason <jason90814@gmail.com>
2021-07-01 11:00:48 +0800

    add comment

--- School.py
+++ School.py
@@ -5,10 +5,8 @@ class School():
         self.quota = quota
         self.accept = AcceptedStudentHeap()
         self.weighted = weighted
-        #超額比序項目
-        self.overquota = overquota
-        #超額的人數
-        self.extraquota = 0
+        self.overquota = overquota  #超額比序項目
+        self.extraquota = 0         #超額的人數
     def __str__(self):
         return str(self.name)
     def find_weighted(self,student):
@@ -16,7 +14,7 @@ class School():
         for i in range(len(self.weighted)):
             weighted_score += self.weighted[i]*student.score[i]
         return weighted_score
-    #回傳 1 代表 student_1 超額比序輸了
+    #回傳 (0,1,2) 代表 (student_1 贏了,student_1 輸了,完全同分)
     def find_overquota(self,student_1,student_2):
         i = 0
         #完全同分則增額錄取
