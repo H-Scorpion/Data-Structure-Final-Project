061d049141d45d6085e33cd2a1741fae1e3514ec
YellowJason <jason90814@gmail.com>
2021-07-02 23:59:10 +0800

    feat: put two plot together

--- plot.py
+++ plot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
-def initialize():
-    with open('runtimeList3.txt','r') as f:
+def initialize(file):
+    with open(file,'r') as f:
         runtimeData = f.readlines()
     studentNum=[]
     runtime=[]
@@ -10,9 +10,12 @@ def initialize():
         studentNum.append(int(data[0]))
         runtime.append(float(data[1]))
     return studentNum,runtime
-studentNum,runtime=initialize()
-plt.plot(studentNum,runtime)
+studentNum_1,runtime_1=initialize('runtimeHeap1.txt')
+studentNum_2,runtime_2=initialize('runtimeList3.txt')
+data1, = plt.plot(studentNum_1,runtime_1,label='using Heap')
+data2, = plt.plot(studentNum_2,runtime_2,label='using List')
 plt.xlabel("studentNum")
 plt.ylabel("runtime")
 plt.title("schoolNum=200,choice=5")
+plt.legend(handles=[data1,data2])
 plt.show()
