3840c63cd6d16f74f3e882bdbb85e3381228470b
H-Scorpion <57944687+H-Scorpion@users.noreply.github.com>
2021-07-04 17:26:32 +0800

    docs: description for calRuntime and plot

--- calRuntime.py
+++ calRuntime.py
@@ -2,18 +2,18 @@ import time
 from generateData import genData
 from main import *
 
-fileName='runtimeHeap1.txt'
+fileName='runtimeHeap.txt'
 if __name__=='__main__':
-    with open(fileName,'w') as f:
+    with open('./outputData/runtimeData/'+fileName,'w') as f:
         f.truncate()
 
-    for studentNum in range(1000,100000,1000):
+    for studentNum in range(10,100000,1000):
         data=genData(200,studentNum,5)
         match=initialize(data)
         print(studentNum)
         t1=time.time()
         match.start_match()
         t2=time.time()
-        with open(fileName,'a') as f:
+        with open('./outputData/runtimeData/'+fileName,'a') as f:
             f.writelines(str(studentNum)+' '+str(t2-t1)+'\n')
     print("complete")
--- plot.py
+++ plot.py
@@ -10,8 +10,8 @@ def initialize(file):
         studentNum.append(int(data[0]))
         runtime.append(float(data[1]))
     return studentNum,runtime
-studentNum_1,runtime_1=initialize('runtimeHeap1.txt')
-studentNum_2,runtime_2=initialize('runtimeList3.txt')
+studentNum_1,runtime_1=initialize('./outputData/runtimeData/runtimeHeap.txt')
+studentNum_2,runtime_2=initialize('./outputData/runtimeData/runtimeList3.txt')
 data1, = plt.plot(studentNum_1,runtime_1,label='using Heap')
 data2, = plt.plot(studentNum_2,runtime_2,label='using List')
 plt.xlabel("studentNum")
